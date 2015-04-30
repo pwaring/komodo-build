6622404fd5ead1891c355f18fb94a6c8a1272496
Paul Waring <paul.waring@manchester.ac.uk>
2015-04-30 13:41:21 +0100

    Move all config options to separate file

--- build.sh
+++ build.sh
@@ -3,9 +3,7 @@
 set -u
 set -e
 
-export KMD_TMP_DIR="/tmp/komodo"
-
-export GNOME_BASE_URL="http://ftp.gnome.org/pub/gnome/sources/"
+source ./config.sh
 
 # Remove and then recreate temporary directory
 if [ -d ${KMD_TMP_DIR} ]; then
@@ -15,9 +13,6 @@ fi
 mkdir ${KMD_TMP_DIR}
 
 # 1. Clone config files so we have up to date versions
-export CONFIG_URL="git://git.savannah.gnu.org/config.git"
-export CONFIG_SRC_DIR="${KMD_TMP_DIR}/config"
-
 # Only clone the repository if it does not exist on disk
 if [ ! -d ${CONFIG_SRC_DIR} ]; then
   cd ${KMD_TMP_DIR}
@@ -25,17 +20,6 @@ if [ ! -d ${CONFIG_SRC_DIR} ]; then
 fi
 
 # 2. Download and build glib
-export GLIB_VERSION_MAJOR="1.2"
-export GLIB_VERSION_MINOR="0"
-export GLIB_VERSION="${GLIB_VERSION_MAJOR}.${GLIB_VERSION_MINOR}"
-export GLIB_FILENAME="glib-${GLIB_VERSION}.tar.gz"
-export GLIB_URL="${GNOME_BASE_URL}/glib/${GLIB_VERSION_MAJOR}/${GLIB_FILENAME}"
-export GLIB_TARBALL="${KMD_TMP_DIR}/${GLIB_FILENAME}"
-export GLIB_SRC_DIR="${KMD_TMP_DIR}/glib-${GLIB_VERSION}"
-export GLIB_CONFIGURE_OPTIONS=(
-  "--disable-glibtest"
-)
-
 # Only download the tarball if it does not exist
 if [ ! -f ${GLIB_TARBALL} ]; then
   wget ${GLIB_URL} -O ${GLIB_TARBALL}
@@ -60,19 +44,6 @@ make
 sudo make install
 
 # 3. Download and build GTK
-export GTK_VERSION_MAJOR="1.2"
-export GTK_VERSION_MINOR="10"
-export GTK_VERSION="${GTK_VERSION_MAJOR}.${GTK_VERSION_MINOR}"
-export GTK_FILENAME="gtk+-${GTK_VERSION}.tar.gz"
-export GTK_URL="${GNOME_BASE_URL}/gtk+/${GTK_VERSION_MAJOR}/${GTK_FILENAME}"
-export GTK_TARBALL="${KMD_TMP_DIR}/${GTK_FILENAME}"
-export GTK_SRC_DIR="${KMD_TMP_DIR}/gtk+-${GTK_VERSION}"
-export GTK_CONFIGURE_OPTIONS=(
-  "--disable-glibtest"
-  "--disable-gtktest"
-  "--with-glib=${GLIB_SRC_DIR}"
-)
-
 # Only download the tarball if it does not exist
 if [ ! -f ${GTK_TARBALL} ]; then
   wget ${GTK_URL} -O ${GTK_TARBALL}
@@ -97,17 +68,6 @@ make
 sudo make install
 
 # 4. Download and build Komodo
-export KMD_VERSION="1.5.0"
-export KMD_FILENAME="kmd.tar.gz"
-export KMD_URL="https://studentnet.cs.manchester.ac.uk/resources/software/komodo/${KMD_FILENAME}"
-export KMD_TARBALL="${KMD_TMP_DIR}/${KMD_FILENAME}"
-export KMD_SRC_DIR="${KMD_TMP_DIR}/KMD-${KMD_VERSION}"
-export KMD_CONFIGURE_OPTIONS=(
-  "--disable-glibtest"
-  "--disable-gtktest"
-  "--with-glib=${GLIB_SRC_DIR}"
-)
-
 # Only download the tarball if it does not exist
 if [ ! -f ${KMD_TARBALL} ]; then
   wget ${KMD_URL} -O ${KMD_TARBALL}
---
+++ config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export KMD_TMP_DIR="/tmp/komodo"
+export GNOME_BASE_URL="http://ftp.gnome.org/pub/gnome/sources/"
+
+export CONFIG_URL="git://git.savannah.gnu.org/config.git"
+export CONFIG_SRC_DIR="${KMD_TMP_DIR}/config"
+
+export GLIB_VERSION_MAJOR="1.2"
+export GLIB_VERSION_MINOR="0"
+export GLIB_VERSION="${GLIB_VERSION_MAJOR}.${GLIB_VERSION_MINOR}"
+export GLIB_FILENAME="glib-${GLIB_VERSION}.tar.gz"
+export GLIB_URL="${GNOME_BASE_URL}/glib/${GLIB_VERSION_MAJOR}/${GLIB_FILENAME}"
+export GLIB_TARBALL="${KMD_TMP_DIR}/${GLIB_FILENAME}"
+export GLIB_SRC_DIR="${KMD_TMP_DIR}/glib-${GLIB_VERSION}"
+export GLIB_CONFIGURE_OPTIONS=(
+  "--disable-glibtest"
+)
+
+export GTK_VERSION_MAJOR="1.2"
+export GTK_VERSION_MINOR="10"
+export GTK_VERSION="${GTK_VERSION_MAJOR}.${GTK_VERSION_MINOR}"
+export GTK_FILENAME="gtk+-${GTK_VERSION}.tar.gz"
+export GTK_URL="${GNOME_BASE_URL}/gtk+/${GTK_VERSION_MAJOR}/${GTK_FILENAME}"
+export GTK_TARBALL="${KMD_TMP_DIR}/${GTK_FILENAME}"
+export GTK_SRC_DIR="${KMD_TMP_DIR}/gtk+-${GTK_VERSION}"
+export GTK_CONFIGURE_OPTIONS=(
+  "--disable-glibtest"
+  "--disable-gtktest"
+  "--with-glib=${GLIB_SRC_DIR}"
+)
+
+export KMD_VERSION="1.5.0"
+export KMD_FILENAME="kmd.tar.gz"
+export KMD_URL="https://studentnet.cs.manchester.ac.uk/resources/software/komodo/${KMD_FILENAME}"
+export KMD_TARBALL="${KMD_TMP_DIR}/${KMD_FILENAME}"
+export KMD_SRC_DIR="${KMD_TMP_DIR}/KMD-${KMD_VERSION}"
+export KMD_CONFIGURE_OPTIONS=(
+  "--disable-glibtest"
+  "--disable-gtktest"
+  "--with-glib=${GLIB_SRC_DIR}"
+)
